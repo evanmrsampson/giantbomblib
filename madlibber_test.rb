f74aa6a4eb42cbbc897e270e06dd98ef8ec4aa98
evanmrsampson <evanmrsampson@gmail.com>
2019-08-29 19:10:22 -0400

    add httparty get 5 responses

--- madlibber_test.rb
+++ madlibber_test.rb
@@ -1,6 +1,24 @@
 require 'madlibber'
 require 'yaml'
+require 'httparty'
+require 'json'
 
-API_KEY = YAML.load(File.read("giantbomb.yaml"))['api_key']
+def get_response(resource, query_string)
+    config = YAML.load(File.read("giantbomb.yaml"))
+    api_key = config['api_key']
+    api_base = config['api_base']
+    return HTTParty.get("#{api_base}/#{resource}/?api_key=#{api_key}&format=json&#{query_string}")
+end
 
-puts API_KEY
+def generate_text_resources()
+    # get total number of games in database
+    upper_limit = JSON.parse(get_response("games", "").body)['number_of_total_results']
+
+    # generate a random id
+    random_id = rand(upper_limit)
+    
+    # get 100 descriptions!
+    return JSON.parse(get_response("games", "offset=#{random_id}&limit=5&field_list=description,aliases").body)
+end
+
+puts generate_text_resources()
